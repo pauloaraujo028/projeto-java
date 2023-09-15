b4ddd9bc1fabe974e90bab94b1942bdc7668a509
Paulo Araújo <pauloaraujo.phz@gmail.com>
2023-09-14 23:00:38 -0300

    feat: initial commit config

---
+++ src/test/java/com/transfersystem/TransfersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transfersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransfersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
